12f85f625f5b939bad8680f74c84353d51a2246a
Doodesch <30825893+DooDesch@users.noreply.github.com>
2022-07-29 20:21:01 +0200

    Fix createrolemenu

--- code/commands/createrolemenu.js
+++ code/commands/createrolemenu.js
@@ -1,57 +1,55 @@
-const { createSelectMenuIfItsMissing } = require("../modules/roles.js");
-const config = require("../config.js");
-const SQLite = require("better-sqlite3");
-const sql = new SQLite(config.sqliteDatabaseFilePath);
+const { createSelectMenuIfItsMissing } = require('../modules/roles.js')
+const config = require('../config.js')
+const SQLite = require('better-sqlite3')
+const sql = new SQLite(config.sqliteDatabaseFilePath)
 
-const { channelMention } = require("@discordjs/builders");
+const { channelMention } = require('@discordjs/builders')
 
 exports.run = async (client, message) => {
-  const rolesChannel = await message.guild.channels.cache.find(
-    (c) => c.name === "roles"
-  );
+    const rolesChannel = await message.guild.channels.cache.find(
+        (c) => c.name === 'roles'
+    )
 
-  // Get the select menu for this channel from the database
-  const selectMenu = await sql
-    .prepare(`SELECT * FROM select_menus WHERE channel_id = ?`)
-    .get(rolesChannel.id);
+    // Get the select menu for this channel from the database
+    const selectMenu = await sql
+        .prepare(`SELECT * FROM select_menus WHERE channel_id = ?`)
+        .get(rolesChannel.id)
 
-  if (selectMenu) {
-    // Check if the channel has a role select menu message
-    const roleSelectMenuMessage = await rolesChannel.messages.cache.find(
-      (m) => m.id === selectMenu?.message_id
-    );
+    if (selectMenu) {
+        // Check if the channel has a role select menu message
+        const roleSelectMenuMessage = await rolesChannel.messages.cache.find(
+            (m) => m.id === selectMenu?.message_id
+        )
 
-    if (roleSelectMenuMessage) {
-      // Delete the message
-      await roleSelectMenuMessage.delete();
+        if (roleSelectMenuMessage) {
+            // Delete the message
+            await roleSelectMenuMessage.delete()
+        }
+
+        // Delete the database entry
+        await sql
+            .prepare(`DELETE FROM select_menus WHERE message_id = ?`)
+            .run(roleSelectMenuMessage.id)
     }
 
-    // Delete the database entry
-    await sql
-      .prepare(`DELETE FROM select_menus WHERE message_id = ?`)
-      .run(roleSelectMenuMessage.id);
-  }
+    await createSelectMenuIfItsMissing(rolesChannel)
 
-  await createSelectMenuIfItsMissing(rolesChannel);
-
-  // Reply that the message has been created
-  await message.channel.send(
-    `${channelMention(
-      rolesChannel.id
-    )} - A new role select menu has been created.`
-  );
-};
+    // Reply that the message has been created
+    await message.channel.send(
+        `${channelMention(rolesChannel.id)} - A new role select menu has been created.`
+    )
+}
 
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: "Administrator",
-};
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: 'Administrator',
+}
 
 exports.help = {
-  name: "createrolemenu",
-  category: "System",
-  description: "Pong!",
-  usage: "createrolemenu",
-};
+    name: 'createrolemenu',
+    category: 'System',
+    description: 'Creates a role select menu for the roles channel.',
+    usage: 'createrolemenu',
+}
